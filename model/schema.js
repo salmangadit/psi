c93566a8fe316860f51e04140611bc1b745dff71
Salman Gadit <salman.gadit@gmail.com>
2013-06-21 16:04:27 +0800

    DB work

--- model/schema.js
+++ model/schema.js
@@ -1,40 +1,10 @@
 var mongoose = require('mongoose');
 var ObjectId = mongoose.Schema.Types.ObjectId;
 
-var user = new mongoose.Schema({
-  id: {type: String, unique: true},
-  qrcode: String,
-  name: String,
-  email:String,
-  rfid:String,
-  entered:Boolean,
-  facebookid:Number,
-  facebooktoken:String,
-  eventid:String
+var logs = new mongoose.Schema({
+  id: {type: Number, unique: true},
+  value: Number,
+  timestamp: {type : Date, default: Date.now },
 });
 
-var events = new mongoose.Schema({
-  id:{type: String, unique: true},
-  event_name:String,
-});
-
-var entries = new mongoose.Schema({
-  id:{type: String, unique: true},
-  rfid:String,
-  location:String,
-  timestamp:{type : Date, default: Date.now },
-  eventid:String,
-});
-
-var votes = new mongoose.Schema({
-  id:{type: String, unique: true},
-  rfid:String,
-  screen_num:Number,
-  item_num:Number,
-  eventid:String
-});
-
-exports.user = user;
-exports.events = events;
-exports.entries = entries;
-exports.votes = votes;
+exports.logs = logs;
